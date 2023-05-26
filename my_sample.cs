44b0206d21737f1629fa8f104610c88c1db89b10
tinoemaumbe <tinoemaumbe@gmail.com>
2023-05-26 15:56:02 +0200

    add

--- my_sample.cs
+++ my_sample.cs
@@ -215,3 +215,137 @@ namespace DailyJournal
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Journal journal = new Journal();
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("1. Write a new entry");
+                Console.WriteLine("2. Display the journal");
+                Console.WriteLine("3. Save the journal to a file");
+                Console.WriteLine("4. Load the journal from a file");
+                Console.WriteLine("5. Quit");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine(journal.GetRandomPrompt());
+                        string prompt = Console.ReadLine();
+                        journal.AddEntry(prompt);
+                        break;
+                    case "2":
+                        journal.DisplayEntries();
+                        break;
+                    case "3":
+                        Console.WriteLine("Enter filename:");
+                        string filename = Console.ReadLine();
+                        journal.SaveToFile(filename);
+                        break;
+                    case "4":
+                        Console.WriteLine("Enter filename:");
+                        filename = Console.ReadLine();
+                        journal.LoadFromFile(filename);
+                        break;
+                    case "5":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+/*
+This program is a daily journal that allows the user to write entries, save them to a file, 
+and load them from a file.
+
+*/
+
+//Program class
+namespace DailyJournal
+{
+    class Program
+    {
+        static List<Entry> journalEntries = new List<Entry>();
+        
+        //Main method
+        //This method is the main method that runs the program
+        //It is a while loop that runs until the user chooses to exit
+        //It has a switch statement that allows the user to choose what they want to do
+        //It has a try catch statement that catches any errors that may occur
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Write a new entry");
+                Console.WriteLine("2. Display the journal");
+                Console.WriteLine("3. Save the journal to a file");
+                Console.WriteLine("4. Load the journal from a file");
+                Console.WriteLine("5. Exit");
+
+                string input = Console.ReadLine();
+                int choice;
+                //Switch case for choosing menu options
+                if (int.TryParse(input, out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            WriteNewEntry();
+                            break;
+                        case 2:
+                            DisplayJournal();
+                            break;
+                        case 3:
+                            SaveJournal();
+                            break;
+                        case 4:
+                            LoadJournal();
+                            break;
+                        case 5:
+                            return;
+                        default:
+                            Console.WriteLine("Invalid option");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+        }
+    }
+}
